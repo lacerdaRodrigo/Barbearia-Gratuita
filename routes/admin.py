da2cd6b560e576fa90162ff68cfb766d3faa9493
lacerdaRodrigo <162244172+lacerdaRodrigo@users.noreply.github.com>
2025-10-22 20:42:29 +0000

    Corrigindo erro

--- routes/admin.py
+++ routes/admin.py
@@ -120,7 +120,7 @@ def listar_usuarios():
         return jsonify({"mensagem": "Erro interno do servidor."}), 500
 
 @blueprint_admin.route('/admin/usuarios/delete', methods=['DELETE'])
-def delete_admin:
+def delete_admin():
     try:
         admins_collection = mongo.db.admins
 
